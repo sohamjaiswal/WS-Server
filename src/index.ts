52e25162d4b5fd3a61b9b8debd15c8bbb2d8ebc7
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-09-19 19:33:07 +0530

    feat: initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import { Elysia } from "elysia";
+import { Elysia, ws } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+    .use(ws())
+    .ws('/ws', {
+        message(ws, message) {
+            console.log('message from:', ws, message)
+            if (typeof message == 'string' && message.startsWith('private')) {
+                ws.send('this message was private!')
+                return
+            }
+            ws.publish('chat', message)
+        },
+        close(ws) {
+            console.log('a client disconnected')
+            ws.close()
+        },
+        open(ws) {
+            ws.subscribe('chat')
+            ws.send('welcome to the server!')
+            console.log('new connection')
+        }
+    })
+    .listen(8080)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
